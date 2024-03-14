cf4b7f6e254a829e34940f9dcf2e9bbf69d030e4
KanaAishiro <sonicsoliar@gmail.com>
2024-03-14 23:38:59 +0100

    finished active element exposing on hover

--- js/contactsFunctions.js
+++ js/contactsFunctions.js
@@ -1,26 +1,42 @@
-let activeElementId = null;
 
 function onHoverAnimation(id) {
+
     const cardContainer = document.querySelector(".contacts");
     const activeElement = document.getElementById(id);
     const cards = cardContainer.querySelectorAll(`.small-card:not(#${id})`);
 
-    console.log(cards);
+    cards.forEach(card => {
+        card.addEventListener('transitionend', () => {
+            exposeActiveElement(card, activeElement);
+        }, 
+        {once: true});
+    });
+    cards.forEach(card => {
+        card.classList.add("visually-hidden");
+    });
+}
+function exposeActiveElement(card,activeElement) {   
 
-    cards.forEach(card => card.addEventListener("transitionend", exposeActiveElement(card, activeElement), {once: true}));
-    cards.forEach(card => card.classList.add("visually-hidden"));
+    card.classList.add("hidden");
+    if(!activeElement.classList.contains("selected")) {
+        activeElement.addEventListener("transitionend", () => {
+            showActiveElementText(activeElement.children[1]);
+        }, 
+        {once: true});
+        activeElement.classList.add("selected");
+    }
+    
+}
+function showActiveElementText(activeElementText) {
+    activeElementText.classList.remove("hidden");
+    setTimeout(() => activeElementText.classList.remove("visually-hidden"), 50 );
 }
 
 function returnToInitialState() {
-    
-}
+    const cardContainer = document.querySelector(".contacts");
+    const activeElement = document.querySelector(".selected")
+    const cards = cardContainer.querySelectorAll(`.small-card:not(.selected)`);
+    const activeElementText = activeElement.children[1];
 
-function exposeActiveElement(card, activeElement)
-{   
-    this.
-    card.classList.add("hidden");
-    if(!activeElement.classList.contains("selected")) {
-        
-    }
     
 }
