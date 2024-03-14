b2765dea8db87a983d255a8de5310d82019b9fce
KanaAishiro <sonicsoliar@gmail.com>
2024-03-14 15:00:18 +0100

    New version of transition animation on contacts card (WIP)

--- js/contactsFunctions.js
+++ js/contactsFunctions.js
@@ -1,48 +1,26 @@
 let activeElementId = null;
 
 function onHoverAnimation(id) {
-    console.log("on hover animation");
     const cardContainer = document.querySelector(".contacts");
-    let activeElement = document.getElementById(id);
-    const activeElementText = activeElement.querySelector(".small-text-wrapper");
-    activeElementId = id;
-    console.log(activeElement);
-    for(const child of cardContainer.children) {   
-        if(child != activeElement) {
-            child.addEventListener("transitionend", function () {
-                child.classList.add("hidden");
-                activeElement.addEventListener("transitionend", function () {
-                    activeElementText.classList.remove("hidden");
-                    activeElementText.classList.remove("visually-hidden");
-                });
-                activeElement.classList.add("selected");
-            });
-            child.classList.add("visually-hidden");
-        }
-    }
-}
-function returnToInitialState() {
-    console.log("return to init state");
-    const cardContainer = document.querySelector(".contacts");
-    let activeElement = document.getElementById(activeElementId);
-    const activeElementText = activeElement.querySelector(".small-text-wrapper");
-    
-    activeElementText.addEventListener("transitionend", function () {
-        activeElementText.classList.add("hidden");
-        activeElement.classList.remove("selected");
-        console.log("element hides");
-        console.log(activeElementText);
+    const activeElement = document.getElementById(id);
+    const cards = cardContainer.querySelectorAll(`.small-card:not(#${id})`);
 
-        activeElement.addEventListener("transitionend", function () {
-            for(const child of cardContainer.children) {
-                child.classList.remove("hidden");
-                child.classList.remove("visually-hidden");
-                
-            }
-        });
-    });
-    activeElementText.classList.add("visually-hidden"); 
-    console.log(activeElement);
-    console.log(activeElementText);
+    console.log(cards);
+
+    cards.forEach(card => card.addEventListener("transitionend", exposeActiveElement(card, activeElement), {once: true}));
+    cards.forEach(card => card.classList.add("visually-hidden"));
+}
+
+function returnToInitialState() {
+    
+}
+
+function exposeActiveElement(card, activeElement)
+{   
+    this.
+    card.classList.add("hidden");
+    if(!activeElement.classList.contains("selected")) {
+        
+    }
     
 }
