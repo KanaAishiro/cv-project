84ea98b78c07d441804ea26509e2433a734dbba3
KanaAishiro <sonicsoliar@gmail.com>
2024-03-23 21:14:52 +0100

    fixed cotacts tab animations (now fully working)

--- js/contactsFunctions.js
+++ js/contactsFunctions.js
@@ -1,53 +1,18 @@
+const smallCards = document.querySelectorAll(".small-card");
 
-function onHoverAnimation(id) {
 
-    const cardContainer = document.querySelector(".contacts");
-    const activeElement = document.getElementById(id);
-    const cards = cardContainer.querySelectorAll(`.small-card:not(#${id})`);
-    console.log("onhover initiated");
-    cards.forEach(card => {
-        card.addEventListener('transitionend', () => {
-            exposeActiveElement(card, activeElement);
-        }, 
-        {once: true});
+smallCards.forEach(card => {
+    card.addEventListener('mouseenter', () => {
+    smallCards.forEach(i => {
+      if (i !== card) {
+        i.classList.add('hidden');
+      }
     });
-    cards.forEach(card => {
-        card.classList.add("visually-hidden");
+  });
+
+  card.addEventListener('mouseleave', () => {
+    smallCards.forEach(i => {
+      i.classList.remove('hidden');
     });
-}
-function exposeActiveElement(card,activeElement) {   
-
-    card.classList.add("hidden");
-    if(!activeElement.classList.contains("selected")) {
-        activeElement.addEventListener("transitionend", () => {
-            showElement(activeElement.children[1]);
-        }, 
-        {once: true});
-        activeElement.classList.add("selected");
-    }
-    
-}
-function showElement(element) {
-    element.classList.remove("hidden");
-    setTimeout(() => element.classList.remove("visually-hidden"), 50 );
-}
-
-function returnToInitialState() {
-    const cardContainer = document.querySelector(".contacts");
-    const activeElement = document.querySelector(".selected")
-    const cards = cardContainer.querySelectorAll(`.small-card:not(.selected)`);
-    const activeElementText = activeElement.children[1];
-    console.log(activeElement);
-
-    activeElementText.addEventListener("transitionend", () => {
-        activeElementText.classList.add("hidden");
-        activeElement.classList.remove("selected");
-    },
-    {once: true});
-    
-    activeElement.addEventListener("transitionend", () => {
-        cards.forEach(card => showElement(card));
-    },
-    {once: true});
-    activeElementText.classList.add("visually-hidden");
-}
+  });
+});
